e6a4a3eda120ae5a28a4b4e37a7111056eecda0f
Kathange <125526244+Kathange@users.noreply.github.com>
2023-08-01 14:58:50 +0800

    Add files via upload

---
+++ interface/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+# 後方新增 你所建立的 "類別名稱"，這裡我的名稱是BuffetCaloriesAdmin
+from .models import BuffetCalories, BuffetCaloriesAdmin
+
+# Register your models here.
+# 將管理者類別 VendorAdmin 填至該類別後方
+# admin.site.register(BuffetCalories, BuffetCaloriesAdmin)
+
